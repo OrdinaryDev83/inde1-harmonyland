52f02ece2695c6ece51c082865f8f4352d039372
Tom Genlis <bryckout@gmail.com>
2023-06-28 16:14:56 +0200

    fix(drone producer): new json generation based on words and harmonyscore

--- drone_producer/src/main/scala/fr/epita/harmonyland/Main.scala
+++ drone_producer/src/main/scala/fr/epita/harmonyland/Main.scala
@@ -1,22 +1,35 @@
 package fr.epita.harmonyland
 
-import com.google.gson.GsonBuilder
+import net.liftweb.json._
+import net.liftweb.json.Serialization.write
 
-import java.util.concurrent.{Executors, TimeUnit}
+import java.util.concurrent.{CountDownLatch, Executors, TimeUnit}
 
-object Main {
-  private val gson = new GsonBuilder().setPrettyPrinting().create()
+object Main extends App {
+
+  val producer = KafkaProducerApp.create_producer()
+
   private val scheduler = Executors.newScheduledThreadPool(1)
 
   scheduler.scheduleAtFixedRate(
     () => {
-      List.range(0, 10)
+      implicit val formats = DefaultFormats
+      val json_list = List.range(0, 10)
         .map(Simulation.generateReport)
-        .map(gson.toJson)
-        .foreach(println)
+        .map((report) => {
+          val json = write(report)
+          println(json)
+          json
+        })
+      KafkaProducerApp.send(producer, json_list)
     },
     0,
-    10,
+    60,
     TimeUnit.SECONDS
   )
+  // Keep the main thread alive
+  val latch = new CountDownLatch(1)
+  latch.await()
+
+  scheduler.shutdown()
 }
