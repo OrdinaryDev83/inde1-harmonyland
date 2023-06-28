a85644bb75506e932dddf2b5beff4c3b43a9c821
Tom Genlis <bryckout@gmail.com>
2023-06-28 12:05:19 +0200

    fix(discord bot): wrapped up code into functions and added a thread

--- discord_bot/src/main/scala/fr/epita/harmonyland/Actions.scala
+++ discord_bot/src/main/scala/fr/epita/harmonyland/Actions.scala
@@ -11,17 +11,34 @@ import java.util.Properties
 import scala.collection.JavaConverters.{iterableAsScalaIterableConverter, seqAsJavaListConverter}
 
 object Actions {
-  def sendMessage(dotenv : Dotenv, client: DiscordClient, message: String): Unit = {
-    client.onEventSideEffects {
-      implicit c => {
-        case msg: APIMessage.Ready => {
-          println("Ready")
-          val channelId = TextChannelId(dotenv.get("CHANNEL_ID").toLong)
-          val createMessageData = CreateMessageData(message)
-          val createMessageRequest = CreateMessage(channelId, createMessageData)
-          client.requestsHelper.run(createMessageRequest)
-        }
-      }
-    }
+  def sendMessage(cache : MemoryCacheSnapshot, dotenv: Dotenv, client: DiscordClient, message: String): Unit = {
+
+    val channelId = TextChannelId(dotenv.get("CHANNEL_ID").toLong)
+    val createMessageData = CreateMessageData(message)
+    val createMessageRequest = CreateMessage(channelId, createMessageData)
+    println("Trying to send message")
+    client.requestsHelper.run(createMessageRequest)(cache)
+  }
+
+  def build_consumer() : KafkaConsumer[String, String] = {
+    val props: Properties = new Properties()
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "discordConsumersGroup")
+
+    val consumer = new KafkaConsumer[String, String](props)
+    consumer.subscribe(List("alert").asJava)
+    consumer
+  }
+
+  def poll(dotenv : Dotenv, client : DiscordClient, msg : APIMessage.GuildCreate, consumer : KafkaConsumer[String, String]) : Unit = {
+    val records: ConsumerRecords[String, String] = consumer.poll(java.time.Duration.ofSeconds(1))
+    records.asScala.foreach(record => {
+      val s = s"offset = ${record.offset()}, key = ${record.key()}, value = ${record.value()}"
+      Actions.sendMessage(msg.cache.current, dotenv, client, record.value())
+      println(s)
+    })
+    consumer.commitSync()
   }
 }
